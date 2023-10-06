c8411cdc59ac83833a078f28d67be6f1b3913299
DungHB <dunghbph46428@fpt.edu.vn>
2023-10-06 11:12:41 +0700

    demo1

---
+++ src/demo_git1/Demo_Git1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo_git1;
+
+/**
+ *
+ * @author Hi Windows 10
+ */
+public class Demo_Git1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Xin chào");
+    }
+    
+}
